186121169a924d14422860ca59538605b8a2641e
Kenny <kenhufford@gmail.com>
2020-03-03 17:24:46 -0800

    session

--- frontend/components/auth/SessionForm.jsx
+++ frontend/components/auth/SessionForm.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import { Link } from 'react-router-dom';
 
 export default class SessionForm extends Component {
     constructor(props) {
@@ -29,43 +30,48 @@ export default class SessionForm extends Component {
     render(){
         const {username, password, email, firstName, lastName} = this.state
         return (
-            <div>
-              <ul>
-                  {
-                      this.props.errors.map(error => {
-                        return <li>{error}</li>
-                      })
-                  }
-              </ul>
-            <h1>{this.props.formType}</h1>
+          <div className="session-div">
+            <div className="big-text">Sign Up</div>
+            <div className="side-by-side">
+              <p className="small-text">Already have an account?</p>
+              <Link
+                className="link-text" 
+                to="/login">Log in</Link>
+            </div>
+            <ul>
+              {this.props.errors.map(error => {
+                return <li>{error}</li>
+              })}
+            </ul>
+
             <form onSubmit={this.handleSubmit}>
-              {this.props.formType === "Signup" ? 
-              <div>
+              {this.props.formType === "Signup" ?
+                <div className="top-to-bottom">
                   <label>
                     Email
-                    <input type="text" value={email} onChange={this.handleInput("email")} />
-                  </label> 
+                  <input type="text" value={email} onChange={this.handleInput("email")} />
+                  </label>
                   <label>
                     First Name
-                    <input type="text" value={firstName} onChange={this.handleInput("firstName")} />
-                  </label> 
+                  <input type="text" value={firstName} onChange={this.handleInput("firstName")} />
+                  </label>
                   <label>
                     Last Name
-                    <input type="text" value={lastName} onChange={this.handleInput("lastName")} />
-                  </label> 
-              </div>
-                :  
-              <div></div>
+                  <input type="text" value={lastName} onChange={this.handleInput("lastName")} />
+                  </label>
+                </div>
+                :
+                <div></div>
               }
               <label>
                 Username
-                <input type="text" value={username} onChange={this.handleInput("username")} />
+              <input type="text" value={username} onChange={this.handleInput("username")} />
               </label>
               <label>
                 Password
-                <input type="password" value={password} onChange={this.handleInput("password")}/>
+              <input type="password" value={password} onChange={this.handleInput("password")} />
               </label>
-              <input type="submit" value={this.props.formType}/>
+              <input type="submit" value={this.props.formType} />
             </form>
           </div>
         )
